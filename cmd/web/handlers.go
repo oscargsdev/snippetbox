9738de6a169f68561c97712de6a283bb5942a288
Oscar GS <oscargs.dev@gmail.com>
2025-09-11 21:13:24 -0600

    Missing condition: Id should not be less than 1

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,7 +13,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
+	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
