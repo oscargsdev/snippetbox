444c87860b8294252f522af56a4b3c812ebb50d7
Oscar GS <oscargs.dev@gmail.com>
2025-11-11 18:23:34 -0600

    Using template actions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/oscargsdev/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
