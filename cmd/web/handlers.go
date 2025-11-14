e117ea5bdc59e17d910bd25e6bc66d5d5d1e4edb
Oscar GS <oscargs.dev@gmail.com>
2025-11-14 16:32:41 -0600

    Using justinas/alice for composable middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,7 +10,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	panic("oops! something went wrong")
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
