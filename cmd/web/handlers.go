706400d50e341ef2180f1c98392a00214154419a
Oscar GS <oscargs.dev@gmail.com>
2025-11-18 10:38:17 -0600

    Displaying a popup using session manager

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,7 +51,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, r, http.StatusOK, "view.tmpl.html", data)
-
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -90,6 +89,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, r, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 
