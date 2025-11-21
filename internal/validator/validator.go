96c6813cd07d065964484e0db0bc48acbca59d38
Oscar GS <oscargs.dev@gmail.com>
2025-11-20 21:21:57 -0600

    Updated signup logic to register user

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,10 +1,13 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string
@@ -40,4 +43,12 @@ func MaxChars(value string, max int) bool {
 
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
+}
+
+func MinChars(value string, min int) bool {
+	return utf8.RuneCountInString(value) >= min
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
